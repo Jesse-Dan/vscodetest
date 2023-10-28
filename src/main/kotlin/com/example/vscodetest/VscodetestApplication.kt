8ae71fbaf4eba57a3d404117ddeb461d7de52255
Finn Jesse Dan <83787516+Jesse-Dan@users.noreply.github.com>
2023-10-28 19:55:02 +0100

    initial commit

---
+++ src/main/kotlin/com/example/vscodetest/VscodetestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.vscodetest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VscodetestApplication
+
+fun main(args: Array<String>) {
+	runApplication<VscodetestApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/vscodetest/VscodetestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.vscodetest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VscodetestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
